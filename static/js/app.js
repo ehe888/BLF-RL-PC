b0a0e75e5e8792061c2bfd20216258c6ca643da9
ehe888 <eric.leihe@gmail.com>
2015-02-27 01:16:35 +0800

    a lot of improvement

--- static/js/app.js
+++ static/js/app.js
@@ -27,11 +27,11 @@ var resetHotspot = function(){
     var hsh = 47/700 * $(window).height(),
         hsw = hsh;
     /*
-    $("#hotspot-1").css({
-        "left": 0.15446500402253 * actualWidth + "px",
-        "top" : 432/700 * ($(window).height() >= 700 ? $(window).height() : 700)  + "px"
-    });
-    */
+       $("#hotspot-1").css({
+       "left": 0.15446500402253 * actualWidth + "px",
+       "top" : 432/700 * ($(window).height() >= 700 ? $(window).height() : 700)  + "px"
+       });
+       */
 
     $(".hotspot").each(function(){
         $(this).width(hsw).height(hsh);
@@ -46,11 +46,11 @@ var resetHotspot = function(){
 var resetHotspotCt = function(){
     $(".hotspot-ct > div").each(function(){
         var w = $(this).attr("width"),
-            h = $(this).attr("height"),
-            nh = h/bgHeight * $(window).height(),
-            nw = nh * w/h,
-            ntop = ($(window).height() - nh)/2;
-        $(this).height(nh).width(nw).css({"top": ntop + "px"});    
+    h = $(this).attr("height"),
+    nh = h/bgHeight * $(window).height(),
+    nw = nh * w/h,
+    ntop = ($(window).height() - nh)/2;
+    $(this).height(nh).width(nw).css({"top": ntop + "px"});    
     })
 }
 
@@ -96,13 +96,15 @@ var wheeldelta = {
     x: 0,
     y: 0
 };
-var wheeling;
+var wheeling, leftOffset;
 
 $(window).on('mousewheel', function (e) {
     e.preventDefault();
     if (!wheeling) {
         console.log('start wheeling!');
-        $(".sitting-dog").fadeOut();
+        //get starting position of offset.left
+        var offset = $(".site-content").offset(),
+    leftOffset = offset.left;
     }
 
     clearTimeout(wheeling);
@@ -114,13 +116,25 @@ $(window).on('mousewheel', function (e) {
         wheeldelta.x = 0;
         wheeldelta.y = 0;
         //position the dog
-        var offset = $(".site-content").offset();
+        var offset = $(".site-content").offset(),
+             dogPace = leftOffset - offset.left;
+    if(dogPace !== 0){
         if(Math.abs(offset.left)/actualWidth >= 699/bgWidth){ //dog's leftmost position is 699
-            $(".sitting-dog").fadeIn();
+            $(".sitting-dog").show();
         }else{
-            $(".sitting-dog").fadeOut();   
+            $(".sitting-dog").hide();   
         }
+    }
     }, 250);
+
+    var offset = $(".site-content").offset();
+        if(Math.abs(offset.left)/actualWidth < 699/bgWidth){ //dog's leftmost position is 699
+            $(".sitting-dog").fadeOut();
+        }else{
+            if(!$(".sitting-dog").is(":visible")){
+                $(".sitting-dog").fadeIn();
+            }
+        }
 
     wheeldelta.x += e.deltaFactor * e.deltaX;
     wheeldelta.y += e.deltaFactor * e.deltaY;
@@ -139,5 +153,25 @@ $(function(){
 
     $(".hotspot-close").click(function(e){
         $(this).parent().parent().fadeOut();
+    });
+
+    $(".mag-nav-mask").mouseover(function(){
+        var index = $(this).attr("val");
+        $(".mag-page-ct.active").removeClass("active").addClass("inactive");
+        $("#mag-page-"+index).removeClass("inactive").addClass("active");
+    });
+    /*
+    $(".mag-nav-mask").mouseout(function(){
+        $(".mag-page-ct.active").removeClass("active").addClass("inactive");
+        $("#mag-page-0").removeClass("inactive").addClass("active");
+    });*/
+
+    /* hotspot 2-2 product development next page and prev page handler */
+    $(".next-page-arrow").click(function(e){
+        var marginLeft = "-" + $(this).parent().width() + "px";
+        $('.hs2-2-slide-wrapper').animate({marginLeft: marginLeft},600);
+    });
+    $(".prev-page-arrow").click(function(e){
+        $('.hs2-2-slide-wrapper').animate({marginLeft: "0"},600);
     });
 });

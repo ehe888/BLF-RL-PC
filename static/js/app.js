c7ec326d5739f0126c72dc9ac6ecc9977ca08ba0
ehe888 <eric.leihe@gmail.com>
2015-03-03 18:39:49 +0800

    title fix

--- static/js/app.js
+++ static/js/app.js
@@ -339,7 +339,7 @@ $(function(){
             $(".video-2-3-title span").css({ 'font-size': Math.round($(".video-2-3-title").height()/3) + "px" });
             $(".left-video-title").css({ 'font-size': Math.round($(".video-2-3-title").height()/3 * 1.2) + "px" });
             $(".video-2-4-title span").css({ 'font-size': Math.round($(".video-2-4-title").height()/2) + "px" });
-            $(".left-video-title").css({ 'font-size': Math.round($(".video-2-4-title").height()/2) + "px" });
+            $(".left-video-title1").css({ 'font-size': Math.round($(".video-2-4-title").height()/2) + "px" });
         });        
     });
 
